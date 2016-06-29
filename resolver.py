2f37f0c4c51b222834e8d954fee1daa5d3932830
linyang <linyang95@aol.com>
2016-06-29 15:52:45 +0800

    update

--- resolver.py
+++ resolver.py
@@ -1,3 +1,19 @@
+"""
+   Copyright 2016 Yang Lin <linyang95@aol.com>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
 from query import decode
 import logging
 from error import NotSupportException
@@ -21,7 +37,7 @@ class Resolver(object):
 
         try:
             return self.response.code()
-        except NotSupportException, e:
+        except NotSupportException as e:
             logging.error(e.message)
             self.make_not_found_response()
             return self.response.code()
@@ -52,4 +68,4 @@ class Resolver(object):
             self.response.RCODE = CODE_OK
             self.response.RDLENGTH = 4
         else:
-            raise NotSupportException("not support QTYPE: " + str(self.TYPE))
+            raise NotSupportException("not support QTYPE: " + str(self.query.QTYPE))
